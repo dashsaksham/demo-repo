74f384b80d1eeb6e685b2abf3475181e3072f6d5
Saksham Raheja <sakshamraheja@gmail.com>
2020-11-19 16:16:17 +0530

    Add Old.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
 import New from "./components/New";
+import Old from "./components/Old";
 
 function App() {
 	return (
 		<div className="App">
 			<New />
+			<Old />
 		</div>
 	);
 }
---
+++ src/components/Old.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Old() {
+	return (
+		<div>
+			<h1>Old.js</h1>
+		</div>
+	);
+}
+
+export default Old;
